22d25f24f0b138527b11bd866dd50a8443f8d037
vagrant <vagrant@homestead>
2016-08-05 09:48:10 +0000

    first commit with bsn directive

---
+++ angular-bsn.js
@@ -0,0 +1,42 @@
+angular.module('angular-bsn', [])
+
+/* Validates bsn number with the 11-proef */
+.directive('bsn', [function() {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function(scope, element, attrs, $ngModelCtrl) {
+      $ngModelCtrl.$parsers.push(function(viewValue) {
+        var bsn = viewValue;
+        var rest = viewValue;
+        var sum = 0;
+
+        if(bsn <= 99999999 || bsn > 9999999999) {
+          console.log('invalid length');
+          $ngModelCtrl.$setValidity('bsn', false);
+        }else{
+          sum += (rest % 10) * -1;
+          rest = Math.floor(rest / 10);
+
+          for(var multiplier=2; rest > 0; multiplier++) {
+            sum += (rest % 10) * multiplier;
+            rest = Math.floor(rest / 10);
+          }
+
+          if(sum % 11 === 0){
+            console.log('valid bsn');
+            $ngModelCtrl.$setValidity('bsn', true);
+          }else{
+            console.log('invalid bsn');
+            $ngModelCtrl.$setValidity('bsn', false);
+          }
+        }
+
+        return viewValue;
+        
+      });
+    }
+  }
+}]);
+
+
